bcbd8bed3224d6f1c283d295a8a01143164d3797
Seab <honeyboyseab@gmail.com>
2016-02-23 18:43:18 -0500

    added Fermat's theorem to test primality

---
+++ 1.22.rkt
@@ -0,0 +1,65 @@
+#lang planet neil/sicp
+
+;; test whether number is prime as follows
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+(define (timed-prime-test n)
+  (newline) (display n) (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elasped-time)
+  (display " *** ") (display elasped-time))
+
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= remainder b a) 0)
+
+(define (square x) (* x x))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (expmod base exp m)
+  (cond ((= exp 0)
+         1)
+        ((even? exp)
+         (remainder
+          (square
+           (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base
+             (expmod base (- exp 1) m))
+          m))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 7 5)
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+(prime? 3)
--- 1.3.rkt
+++
@@ -1,9 +0,0 @@
-#lang planet neil/sicp
-
-(define (square x) (* x x))
-(define (square-larger-of-two a b c)
-    (cond ((= a (min a b c))
-          (+ (square b) (square c)))
-          ((= b (min a b c))
-          (+ (square a) (square c)))
-          (else (+ (square a) (square b)))))
