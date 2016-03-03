238c851e1e3f5b72950734bd65556fa6b2945ca9
Seab <honeyboyseab@gmail.com>
2016-03-03 15:58:43 -0500

    updated how to use procedures as arguments in Scheme

--- proceduresAsArguments.rkt
+++ proceduresAsArguments.rkt
@@ -11,11 +11,11 @@
       (+ a (sum-integers (+ a 1) b))))
 
 ;; computes the sum of the cubes  of the integers in the given range
-(define (sum-cubes a b)
-  (if (> a b)
-      0
-      (+ (cube a)
-         (sum-cubes (+ a 1) b))))
+;(define (sum-cubes a b)
+;  (if (> a b)
+;      0
+;     (+ (cube a)
+;        (sum-cubes (+ a 1) b))))
 
 ;; computes the sum of the sequence of terms in a series...which converges to pi/8
 (define (pi-sum a b)
@@ -23,4 +23,17 @@
       0
       (+ (/ 1.0 (* a (+ a 2)))
          (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
+
 
